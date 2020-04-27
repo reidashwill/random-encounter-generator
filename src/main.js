9f712ae998149872117d1e57890e0dc4d15642c7
Daniel Merys <dkmerys@gmail.com>
2020-04-27 10:39:15 -0700

    add printout of user input to html and add css styling to page

--- src/main.js
+++ src/main.js
@@ -5,14 +5,15 @@ import $ from "jquery";
 
 $(document).ready(function () {
   $("#userInput").submit(function (event) {
-    let partyLevel = $(partyLevel).val();
-    let partyMembers = $(partyMembers).val();
-    let challengeDifficulty = $(challengeDifficulty).val();
-    let monsterNumber = $(monsterNumber).val();
-    let monsterSize = $(monsterSize).val();
-    let monsterType = $(monsterType).val();
-    let environment = $(environment).val();
     event.preventDefault();
+    let partyLevel = $("#partyLevel").val();
+    let partyMembers = $("#partyMembers").val();
+    let challengeDifficulty = $("#challengeDifficulty").val();
+    let monsterNumber = $("#monsterNumber").val();
+    let monsterSize = $("#monsterSize").val();
+    let monsterType = $("#monsterType").val();
+    let environment = $("#environment").val();
 
+    $(".col-md-8").html(`<p>Your party level is ${partyLevel}. You have ${partyMembers} members. Your encounter difficulty is set to ${challengeDifficulty}. You will be facing ${monsterNumber} monsters. They are  ${monsterSize} size. The type you are facing is ${monsterType}. Your environment is ${environment} </p>`)
   });
 });
