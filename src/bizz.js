da9b464fc4c9d3444cbba4c6fbaedd98ee972660
Daniel Merys <dkmerys@gmail.com>
2020-04-27 15:26:27 -0700

    update syntax errors and add background change functionality

--- src/bizz.js
+++ src/bizz.js
@@ -1,19 +1,21 @@
 export class Encounter {
-  constructor(partyLevel, partyNumber, difficulty, xpThreshhold) {
+  constructor(partyLevel, partyNumber, difficulty, xpThreshold) {
     this.partyLevel = partyLevel;
     this.partyNumber = partyNumber;
     this.difficulty = difficulty;
-    this.xpThreshhold = xpTheshhold;
+    this.xpThreshold = xpThreshold;
+    this.possibleMonsterArray = ["baboon", "badger", "bat", "cat", "commoner", "crab", "deer", "eagle", "frog", "goat"];
+    
   }
-  possibleMonsterArray = [baboon, badger, bat, cat, commoner, crab, deer, eagle, frog, goat];
+  
 
-  encounerArray = []
+
 
   encounterGen(){
-    for(i=0; i<this.xpThreshhold; ++1){
-      let monsterSelectNum = this.randomNumber(possibleMonsterArray.length);
-      encounterArray.push(this.possibleMonsterArray[monsterSeclectNum]);
-      this.xpThreshhold -= this.possibleMonsterArray[monsterSeclectNum].xp;
+    for(let i=0; i<this.xpThreshold; i++){
+      let monsterSelectNum = this.randomNumber(this.possibleMonsterArray.length);
+      this.encounterArray.push(this.possibleMonsterArray[monsterSelectNum]);
+      this.xpThreshold -= this.possibleMonsterArray[monsterSelectNum].xp;
     }
   }
 
@@ -34,104 +36,93 @@ export class Encounter {
   randomNumber(max) {
     return Math.floor(Math.random() * Math.floor(max));
   }
-  partyXpThreshhold(partyLevel, difficulty){
-   this.xpThreshhold = this.partyNumber * threshholdArray[partyLevel][difficulty];
+  partyxpThreshold(partyLevel, difficulty){
+    const threshholdArray = [[],
+      [25, 50, 75, 100],
+      [50, 100, 150, 200],
+      [75, 150, 225, 400],
+      [125, 250, 375, 500],
+      [250, 500, 750, 1100],
+      [300, 600, 900, 1400],
+      [350, 750, 1100, 1700],
+      [450, 900, 1400, 2100],
+      [550, 1100, 1600, 2400],
+      [600, 1200, 1900, 2800],
+      [800, 1600, 2400, 3600],
+      [1000, 2000, 3000, 4500],
+      [1100, 2200, 3400, 5100],
+      [1250, 2500, 3800, 5700],
+      [1400, 2800, 4300, 6400],
+      [1600, 3200, 4800, 7200],
+      [2000, 3900, 5900, 8800],
+      [2100, 4200, 6300, 9500],
+      [2400, 4900, 7300, 10900],
+      [2800, 5700, 8500, 12700]
+    ];
+    this.xpThreshold = this.partyNumber * threshholdArray[partyLevel][difficulty];
   }
 
-  threshholdArray = [[],
-    [25, 50, 75, 100],
-    [50, 100, 150, 200],
-    [75, 150, 225, 400],
-    [125, 250, 375, 500],
-    [250, 500, 750, 1100],
-    [300, 600, 900, 1400],
-    [350, 750, 1100, 1700],
-    [450, 900, 1400, 2100],
-    [550, 1100, 1600, 2400],
-    [600, 1200, 1900, 2800],
-    [800, 1600, 2400, 3600],
-    [1000, 2000, 3000, 4500],
-    [1100, 2200, 3400, 5100],
-    [1250, 2500, 3800, 5700],
-    [1400, 2800, 4300, 6400],
-    [1600, 3200, 4800, 7200],
-    [2000, 3900, 5900, 8800],
-    [2100, 4200, 6300, 9500],
-    [2400, 4900, 7300, 10900],
-    [2800, 5700, 8500, 12700]
-  ]
 
   monsterXpConverter(placeholder) {
     if(this.results.placeholder.challenge_rating === "0"){
-      this.xp = 10
+      this.xp = 10;
     }else if(this.challenge_rating ==="1/8"){
-      this.xp = 25
+      this.xp = 25;
     }else if(this.challenge_rating ==="1/4"){
-      this.xp = 50
+      this.xp = 50;
     }else if(this.challenge_rating ==="1/2"){
-      this.xp = 100
+      this.xp = 100;
     }else if(this.challenge_rating ==="1"){
-      this.xp = 200
+      this.xp = 200;
     }else if(this.challenge_rating ==="2"){
-      this.xp = 450
+      this.xp = 450;
     }else if(this.challenge_rating ==="3"){
-      this.xp = 700
+      this.xp = 700;
     }else if(this.challenge_rating ==="4"){
-      this.xp = 1100
+      this.xp = 1100;
     }else if(this.challenge_rating ==="5"){
-      this.xp = 1800
+      this.xp = 1800;
     }else if(this.challenge_rating ==="6"){
-      this.xp = 2300
+      this.xp = 2300;
     }else if(this.challenge_rating ==="7"){
-      this.xp = 2900
+      this.xp = 2900;
     }else if(this.challenge_rating ==="8"){
-      this.xp = 3900
+      this.xp = 3900;
     }else if(this.challenge_rating ==="9"){
-      this.xp = 5000
+      this.xp = 5000;
     }else if(this.challenge_rating ==="10"){
-      this.xp = 5900
+      this.xp = 5900;
     }else if(this.challenge_rating ==="11"){
-      this.xp = 7200
+      this.xp = 7200;
     }else if(this.challenge_rating ==="12"){
-      this.xp = 8400
+      this.xp = 8400;
     }else if(this.challenge_rating ==="13"){
-      this.xp = 10000
+      this.xp = 10000;
     }else if(this.challenge_rating ==="14"){
-      this.xp = 11500
+      this.xp = 11500;
     }else if(this.challenge_rating ==="15"){
-      this.xp = 13000
+      this.xp = 13000;
     }else if(this.challenge_rating ==="16"){
-      this.xp = 15000
+      this.xp = 15000;
     }else if(this.challenge_rating ==="17"){
-      this.xp = 18000
+      this.xp = 18000;
     }else if(this.challenge_rating ==="18"){
-      this.xp = 20000
+      this.xp = 20000;
     }else if(this.challenge_rating ==="19"){
-      this.xp = 22000
+      this.xp = 22000;
     }else if(this.challenge_rating ==="20"){
-      this.xp = 25000
+      this.xp = 25000;
     }else if(this.challenge_rating ==="21"){
-      this.xp = 33000
+      this.xp = 33000;
     }else if(this.challenge_rating ==="22"){
-      this.xp = 41000
+      this.xp = 41000;
     }else if(this.challenge_rating ==="23"){
-      this.xp = 50000
+      this.xp = 50000;
     }else if(this.challenge_rating ==="24"){
-      this.xp = 62000
+      this.xp = 62000;
     }else if(this.challenge_rating ==="30"){
-      this.xp = 155000
+      this.xp = 155000;
     }
   }
 }
-
-// monster1Attack(monster2) {
-
-//   var roll = (Math.floor(Math.random() * 20) + 1);
-//   this.attackPower = (this.attackBonus + roll);
-
-//   if(this.attackPower > monster2.ac){
-//     monster2.hitPoints -= this.damage;
-//   }
-// }
-
 
--- src/main.js
+++ src/main.js
@@ -3,32 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
 
-import { Encounter } from './src/bizz.js';
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+import { Encounter } from './../src/bizz.js';
 
 
 $(document).ready(function () {
   let environment = $("#environment").val();
-  $("input#environment:checked").click(function() {
-    let imageUrl = "http://getwallpapers.com/wallpaper/full/5/d/f/158233.jpg";
-    if (environment === "Forest") {
-    $(".container").css("background-image", "url(" + imageUrl + ")"); 
+
+  $('#environment').change(function() {
+    let selectedValue = parseInt($(this).val());
+    switch(selectedValue) {
+    case 0:
+      defaultBg();
+      break;
+    case 1:
+      forestBg();
+      break;
+    case 2:
+      desertBg();
+      break;
+        
     }
-  }); 
+  });
+  function defaultBg() {
+    let imageUrl = 'https://wallpaperaccess.com/full/1886635.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function forestBg() {
+    let imageUrl = 'http://getwallpapers.com/wallpaper/full/5/d/f/158233.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function desertBg() {
+    let imageUrl = 'https://i.pinimg.com/originals/7e/60/11/7e60112e76df65f349ddaf3734ea68cd.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
   
   $("#form-control").submit(function (event) {
     event.preventDefault();
@@ -46,10 +53,9 @@ $(document).ready(function () {
     // }
     
 
-    $(".col-md-9").html(`<p>Your party level is ${partyLevel}. You have ${partyMembers} members. Your encounter difficulty is set to ${challengeDifficulty}.The type you are facing is ${monsterType}. Your environment is ${environment} </p>`)
+    $(".col-md-9").html(`<p>Your party level is ${partyLevel}. You have ${partyMembers} members. Your encounter difficulty is set to ${challengeDifficulty}.The type you are facing is ${monsterType}. Your environment is ${environment} </p>`);
   });
 });
 
 
 // You will be facing ${ monsterNumber } monsters.
-
