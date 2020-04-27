ff7be8e01bbfa39ac1e061be6380d20b1e77d3af
Daniel Merys <dkmerys@gmail.com>
2020-04-27 09:51:41 -0700

    add user input form to html, add let variables to main.js.

--- src/main.js
+++ src/main.js
@@ -2,3 +2,17 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+
+$(document).ready(function () {
+  $("#userInput").submit(function (event) {
+    let partyLevel = $(partyLevel).val();
+    let partyMembers = $(partyMembers).val();
+    let challengeDifficulty = $(challengeDifficulty).val();
+    let monsterNumber = $(monsterNumber).val();
+    let monsterSize = $(monsterSize).val();
+    let monsterType = $(monsterType).val();
+    let environment = $(environment).val();
+    event.preventDefault();
+
+  });
+});
