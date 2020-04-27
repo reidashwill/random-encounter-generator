26a4a4ab0ae7d7ea78cdf6e8cc096c1d0f3643c8
Reid Ashwill <reidashwill@gmail.com>
2020-04-27 09:01:32 -0700

    initialize project

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: '',
+      title: 'random-encounter-generator',
       template: './src/index.html',
       inject: 'body'
     })
