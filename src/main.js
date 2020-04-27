b3a76d07c9da7cf511f92cc00d5f6962f5145159
Daniel Merys <dkmerys@gmail.com>
2020-04-27 13:52:43 -0700

    add styling, javascript display function

--- src/main.js
+++ src/main.js
@@ -4,16 +4,33 @@ import './styles.css';
 import $ from "jquery";
 
 $(document).ready(function () {
-  $("#userInput").submit(function (event) {
+  let environment = $("#environment").val();
+  $("input#environment:checked").click(function() {
+    let imageUrl = "http://getwallpapers.com/wallpaper/full/5/d/f/158233.jpg";
+    if (environment === "Forest") {
+    $(".container").css("background-image", "url(" + imageUrl + ")"); 
+    }
+  }); 
+  
+  $("#form-control").submit(function (event) {
     event.preventDefault();
     let partyLevel = $("#partyLevel").val();
     let partyMembers = $("#partyMembers").val();
     let challengeDifficulty = $("#challengeDifficulty").val();
-    let monsterNumber = $("#monsterNumber").val();
-    let monsterSize = $("#monsterSize").val();
+    // let monsterNumber = $("#monsterNumber").val();
+    // let monsterSize = $("#monsterSize").val();
     let monsterType = $("#monsterType").val();
     let environment = $("#environment").val();
+    // $("#environment").on('click', "Forest");
+    // if ($("#environment").val() === "Forest") {
+    //   console.log(environment);
 
-    $(".col-md-8").html(`<p>Your party level is ${partyLevel}. You have ${partyMembers} members. Your encounter difficulty is set to ${challengeDifficulty}. You will be facing ${monsterNumber} monsters. They are  ${monsterSize} size. The type you are facing is ${monsterType}. Your environment is ${environment} </p>`)
+    // }
+    
+
+    $(".col-md-9").html(`<p>Your party level is ${partyLevel}. You have ${partyMembers} members. Your encounter difficulty is set to ${challengeDifficulty}.The type you are facing is ${monsterType}. Your environment is ${environment} </p>`)
   });
 });
+
+
+// You will be facing ${ monsterNumber } monsters.
