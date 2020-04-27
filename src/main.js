84fb574f0d93ca810bb80b05a86ee7b6f9fe1735
Reid Ashwill <reidashwill@gmail.com>
2020-04-27 13:28:16 -0700

    troubleshoot backend logic

--- src/main.js
+++ src/main.js
@@ -2,3 +2,35 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import $ from "jquery";
+import { Encounter } from './src/bizz.js';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(document).ready(function () {
+//   $('#button').click(function (event) {
+//     event.preventDefault();
+//     let inputCurrency = $('#currency').val();
+//     let inputAmount = $('#dollar').val();
+//     (async () => {
+//       let xchange = new Xchange();
+//       let response = await xchange.getData(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
+//       getElements(response.conversion_rates[inputCurrency]);
+//     })();
+//     function getElements(response) {
+//       if (response) {
+//         $('#result').text(parseInt(response * inputAmount));
+
+//         let response = await conversions.apiCall();
+//         let response2 = await conversions.bitCoinApiCall();
