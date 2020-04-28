37d9ea04e2ac8f2bc04fb81c20a51c6939a1d997
Daniel Merys <dkmerys@gmail.com>
2020-04-28 13:10:56 -0700

    add background change functionality on dropdown selection

--- src/main.js
+++ src/main.js
@@ -7,19 +7,48 @@ import { Encounter } from './../src/bizz.js';
 
 
 $(document).ready(function () {
-  let environment = $("#environment").val();
+  let encounter = new Encounter;
+  const aud = document.getElementById("myAudio");
+  aud.volume = 0.5; // default 1 means 100%
   //changes background image upon selection of environment
   $('#environment').change(function () {
-    let selectedValue = parseInt($(this).val());
+    let selectedValue = ($(this).val());
     switch (selectedValue) {
-    case 0:
+    case "default":
       defaultBg();
       break;
-    case 1:
+    case "arcticEncounter":
+      arcticBg();
+      break;
+    case "coastalEncounter":
+      coastalBg();
+      break;
+    case "desertEncounter":
+      desertBg();
+      break;
+    case "forestEncounter":
       forestBg();
       break;
-    case 2:
-      desertBg();
+    case "grasslandEncounter":
+      grasslandBg();
+      break;
+    case "hillEncounter":
+      hillBg();
+      break;
+    case "mountainEncounter":
+      mountainBg();
+      break;
+    case "swampEncounter":
+      swampBg();
+      break;
+    case "underdarkEncounter":
+      underdarkBg();
+      break;
+    case "underwaterEncounter":
+      underwaterBg();
+      break;
+    case "urbanEncounter":
+      urbanBg();
       break;
     }
   });
@@ -27,36 +56,72 @@ $(document).ready(function () {
     let imageUrl = 'https://wallpaperaccess.com/full/1886635.jpg';
     $("body").css("background-image", "url(" + imageUrl + ")");
   }
-  function forestBg() {
-    let imageUrl = 'https://bit.ly/35aNpB9';
+  function arcticBg() {
+    let imageUrl = 'https://i.imgur.com/kxrXtJc.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function coastalBg() {
+    let imageUrl = 'https://i.imgur.com/BbLS9Co.jpg';
     $("body").css("background-image", "url(" + imageUrl + ")");
   }
   function desertBg() {
-    let imageUrl = 'https://bit.ly/3bIhNWh';
+    let imageUrl = 'https://i.imgur.com/fQxvZa3.jpg';
     $("body").css("background-image", "url(" + imageUrl + ")");
   }
-  
-  $("#form-control").submit(function (event) { 
-    event.preventDefault();
-    
-    (async () => {
-      let encounter = new Encounter;
-      let response = await encounter.getList(`https://api.open5e.com/monsters/?challenge_rating=&armor_class=&type=ooze`);
-      let index = encounter.randomNumber(parseInt(response.length));
-      let name = response[index].results.name;
-      console.log(index);
-      console.log(name);
-      encounter.partyLevel = $("#partyLevel").val();
-      encounter.partyMembers = $("#partyMembers").val();
-      encounter.challengeDifficulty = $("#challengeDifficulty").val();
-      encounter.monsterNumber = $("#monsterNumber").val();
-      encounter.monsterSize = $("#monsterSize").val();
-      encounter.monsterType = $("#monsterType").val();
-      encounter.environment = $("#environment").val();
-    })();
-    
+  function forestBg() {
+    let imageUrl = 'https://i.imgur.com/7GlpKVl.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function grasslandBg() {
+    let imageUrl = 'https://i.imgur.com/P4n5fD1.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function hillBg() {
+    let imageUrl = 'https://i.imgur.com/w1UqSXP.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function mountainBg() {
+    let imageUrl = 'https://i.imgur.com/nWyuen4.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function swampBg() {
+    let imageUrl = 'https://i.imgur.com/wRWDzWV.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function underdarkBg() {
+    let imageUrl = 'https://i.imgur.com/ZO5asa3.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function underwaterBg() {
+    let imageUrl = 'https://i.imgur.com/gjZaycz.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  function urbanBg() {
+    let imageUrl = 'https://i.imgur.com/s238XWq.jpg';
+    $("body").css("background-image", "url(" + imageUrl + ")");
+  }
+  // function showDisplay() {
+  //   $(".col-md-9").html(`<p>Your party level is ${encounter.partyLevel}. You have ${encounter.partyMembers} members. Your encounter difficulty is set to ${encounter.challengeDifficulty}.The type you are facing is ${encounter.monsterType}. Your environment is ${encounter.environment} </p>`);
+  // }
 
-    //display for results
-    // $(".col-md-9").html(`<p>Your party level is ${encounter.partyLevel}. You have ${encounter.partyMembers} members. Your encounter difficulty is set to ${encounter.challengeDifficulty}.The type you are facing is ${encounter.monsterType}. Your environment is ${encounter.environment} </p>`);
+  $("#form-control").submit(function (event) {
+    event.preventDefault();
+    $(".results").show();
+
+    // (async () => { // try at returning random index from selected api array
+    //   let response = await encounter.encounterGen;
+    //   let index = encounter.randomNumber(parseInt(response.length));
+    console.log(encounter.partyMembers);
+    console.log(encounter.partyLevel);
+    encounter.partyLevel = $("#partyLevel").val();
+    encounter.partyMembers = $("#partyMembers").val();
+    encounter.challengeDifficulty = $("#challengeDifficulty").val();
+    encounter.monsterNumber = $("#monsterNumber").val();
+    encounter.monsterSize = $("#monsterSize").val();
+    encounter.monsterType = $("#monsterType").val();
+    encounter.environment = $("#environment").val();
+    // })();
+    // showDisplay();
+
   });
 });
